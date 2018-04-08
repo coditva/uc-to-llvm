f689fed2d218b99cc33846fbb1b9d3447b2e6521
UTkarsh Maheshwari <utkarshme96@gmail.com>
2018-04-08 16:45:40 +0530

    Fix test.sh exitcode

--- test.sh
+++ test.sh
@@ -2,13 +2,18 @@
 
 testdir="samples"
 binary="./uc"
+exitcode=0
 
 for testfile in $(ls $testdir) ; do
-    $binary < $testdir/$testfile 2>/dev/null
+
+    $binary < $testdir/$testfile 1>/dev/null 2>&1
 
     if [ $? -eq 0 ]; then
         echo "[PASS] $testfile"
     else
         echo "[FAIL] $testfile"
+        exitcode=1
     fi
 done
+
+exit $exitcode
