87cf55d870ce4bd09d3a833e14b6617a7cb07a42
UTkarsh Maheshwari <utkarshme96@gmail.com>
2018-04-11 22:53:18 +0530

    Make tests verbose when running single test

--- test.sh
+++ test.sh
@@ -3,34 +3,41 @@
 testdir="tests"
 binary="./uc"
 exitcode=0
+verbose=1
 
 function run_test() {
-    err=0
     rm -f a.ll a.bc
 
-    $binary "$testdir/$1" 1>/dev/null 2>&1
-    err+=$?
+    if [[ $verbose == 0 ]]; then $binary "$testdir/$1" 1>/dev/null 2>&1
+    else $binary "$testdir/$1"; fi
+    if [[ $? != 0 ]]; then echo 1; fi
 
-    llvm-as a.ll 1>/dev/null 2>&1
-    err+=$?
+    if [[ $verbose == 0 ]]; then llvm-as a.ll 1>/dev/null 2>&1
+    else llvm-as a.ll; fi
+    if [[ $? != 0 ]]; then echo 1; fi
 
-    lli a.bc 1>/dev/null 2>&1
-
-    if [ $err -eq 0 ]; then
-        echo "[PASS] $1"
-    else
-        echo "[FAIL] $1"
-        exitcode=1
-    fi
+    echo 0
 }
 
 if [[ $TEST != "" ]]; then
-    run_test $TEST
+    if [[ $(run_test $TEST) == 0 ]]; then
+        echo "[PASS] $TEST"
+        exitcode=0
+    else
+        echo "[FAIL] $TEST"
+        exitcode=1
+    fi
     exit $exitcode
 fi
 
 for testfile in $(ls $testdir) ; do
-    run_test $testfile
+    verbose=0
+    if [[ $(run_test $testfile) == 0 ]]; then
+        echo "[PASS] $testfile"
+    else
+        echo "[FAIL] $testfile"
+        exitcode=1
+    fi
 done
 
 exit $exitcode
